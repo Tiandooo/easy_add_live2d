381a4be73b47e59a5ae75a84ba5c7ef193977516
drb-ecust <56426977+drb-ecust@users.noreply.github.com>
2020-06-29 21:57:37 +0800

    Add files via upload

---
+++ addLive2d.js
@@ -0,0 +1,7 @@
+var c = document.createElement("link");
+c.ref = "stylesheet";
+c.href="https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css";
+var s = document.createElement("script");
+s.src="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js";
+document.head.appendChild(c);
+document.body.appendChild(s);
